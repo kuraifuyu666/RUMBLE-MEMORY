41bc0bbb806bcc873ea038b9d43890faf1b482ad
kuraifuyu666 <romain88600@gmail.com>
2024-10-18 15:10:12 +0200

    ajout de firebase et vuefire 🔥

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import './assets/main.css'
-
+import { VueFire, VueFireAuth } from 'vuefire'
+import { firebaseApp } from '@/fireBase'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
@@ -7,6 +8,15 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+
+app.use(VueFire, {
+  // imported above but could also just be created here
+  firebaseApp,
+  modules: [
+    // we will see other modules later on
+    VueFireAuth(),
+  ],
+})
 
 app.use(createPinia())
 app.use(router)
