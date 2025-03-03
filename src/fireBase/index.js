82bee344aab53d0b4a618def058941d0d486400f
kuraifuyu666 <romain88600@gmail.com>
2025-03-03 16:41:26 +0100

    modif

--- src/fireBase/index.js
+++ src/fireBase/index.js
@@ -3,7 +3,7 @@ import { getFirestore, collection } from 'firebase/firestore';
 
 // Configuration de Firebase
 const firebaseConfig = {
-  apiKey: "process.env.REACT_APP_FIREBASE_API_KEY",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: "projet-rumble-memory.firebaseapp.com",
   projectId: "projet-rumble-memory",
   storageBucket: "projet-rumble-memory.appspot.com",
